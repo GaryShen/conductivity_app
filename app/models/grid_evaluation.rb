5bed91288007096ec6484df48adba40cbb41d17f
Gary Shen <gary.shen@itrue.com.tw>
2024-01-21 12:34:10 +0800

    feat() add root path

--- app/models/grid_evaluation.rb
+++ app/models/grid_evaluation.rb
@@ -1,41 +1,41 @@
 class GridEvaluation < ApplicationRecord
-  before_save :check_conductive_path
+  before_save :set_result
 
   def self.random_grid(n)
     Array.new(n) { Array.new(n) { rand(0..1) }.join }.join("\n")
   end
 
-  def check_conductive_path
-    self.result = conductive_path?
-  end
-
-  def conductive_path?
+  def bfs_path
+    path = []
     grid_array = self.grid.split("\n").map { |row| row.split('').map(&:to_i) }
+    visited = Array.new(grid_array.length) { Array.new(grid_array[0].length, false) }
     queue = []
-    max_row = grid_array.length
-    max_col = grid_array[0].length
 
-    # 將第一行中的每個1加入隊列
     grid_array[0].each_with_index do |val, col|
-      queue.push([0, col]) if val == 1
+      if val == 1
+        queue.push([0, col])
+        visited[0][col] = true
+      end
     end
 
-    # 廣度優先搜索
     until queue.empty?
       row, col = queue.shift
+      path << [row, col]
 
-      # 到達底部
-      return true if row == max_row - 1
+      return path if row == grid_array.length - 1
 
-      # 檢查四個方向
-      [[row + 1, col], [row - 1, col], [row, col + 1], [row, col - 1]].each do |new_row, new_col|
-        if new_row.between?(0, max_row - 1) && new_col.between?(0, max_col - 1) && grid_array[new_row][new_col] == 1
+      [[row + 1, col], [row, col + 1], [row - 1, col], [row, col - 1]].each do |new_row, new_col|
+        if new_row.between?(0, grid_array.length - 1) && new_col.between?(0, grid_array[0].length - 1) && grid_array[new_row][new_col] == 1 && !visited[new_row][new_col]
           queue.push([new_row, new_col])
-          grid_array[new_row][new_col] = 0 # 標記已訪問
+          visited[new_row][new_col] = true
         end
       end
     end
 
-    false
+    []
+  end
+
+  def set_result
+    self.result = bfs_path.present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root "grid_evaluations#index"
+
   resources :grid_evaluations, only: %i[index new create show] do
     get :generate_random_grid, on: :collection
   end
